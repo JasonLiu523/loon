d7726f1559562fb3ddcce517bfbc18e7acf3ee94
蔚蓝色星辰 <46687388+JasonLiu523@users.noreply.github.com>
2025-03-21 17:08:29 +0800

    boxjs-beifen.js

--- BoxJS/boxjs-beifen.js
+++ BoxJS/boxjs-beifen.js
@@ -1,10 +1,13 @@
-#!name=backup
+#!name=BoxJS Gist备份和恢复
 #!desc=backup
 #!icon=https://github.com/Toperlock/Quantumult/raw/main/icon/Doraemon/Doraemon-1028.png
 
 [Script]
 # 备份
 cron "55 23 * * *" script-path=https://raw.githubusercontent.com/JasonLiu523/loon/refs/heads/main/BoxJS/backup.js, timeout=120, tag=BoxJS gist备份
+
+# 缓存历史记录
+cron "55 23 * * *" script-path=https://raw.githubusercontent.com/JasonLiu523/loon/refs/heads/main/BoxJS/commit.js, timeout=120, tag=BoxJS gist缓存历史记录
 
 # 恢复
 cron "55 23 * * *" script-path=https://raw.githubusercontent.com/JasonLiu523/loon/refs/heads/main/BoxJS/restore.js, timeout=120, tag=BoxJS gist恢复
